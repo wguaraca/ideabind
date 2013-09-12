0a41bf2e3f6deea05542377615f5cb03e8671734
Edderic Ugaddan <edderic@gmail.com>
2013-09-12 13:17:21 -0400

    Changed config.secret_token to config.secret_key_base'

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Omrails::Application.config.secret_token = 'de8edd51e800a7386acc0f7e634a7a139af7e05bbfb36abc01702a1b7139a5de593f586ef97d26185d6269df840e52e37f7f8f672d895126449021ad2d116833'
+Omrails::Application.config.secret_key_base = 'de8edd51e800a7386acc0f7e634a7a139af7e05bbfb36abc01702a1b7139a5de593f586ef97d26185d6269df840e52e37f7f8f672d895126449021ad2d116833'
