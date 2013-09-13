c0a3692793970b48cd52e75ae2e89cdbe909f1f6
Edderic Ugaddan <edderic@gmail.com>
2013-09-13 13:22:03 -0400

    Added initial testing to pin model

--- spec/models/pins_spec.rb
+++ spec/models/pins_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe Pins do
+describe Pin do
+	let(:user) { FactoryGirl.create(:user) }
+	before do
+		@pin = Pin.new(content: "Loren ipsum", user_id: user.id)
+	end
+
+	subject { @pin }
+
+	it { should respond_to(:description) }
+	it { should respond_to(:user_id) }
 
 end
