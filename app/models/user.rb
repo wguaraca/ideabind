4c5eb5cbec63b2336ad47f09237ab8fe55124df4
Edderic Ugaddan <edderic@gmail.com>
2013-09-12 23:28:51 -0400

    Refactored user_spec.rb and added validations to models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   attr_accessible :title, :body
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+										uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe User do
-	describe "with valid attributes" do
-		before { @user = User.new(
+
+	before { @user = User.new(
 			name: "Example User", 
 			email: "user@example.com",
 			password: "foobar12", 
 			password_confirmation: "foobar12") }
 
-		subject { @user }
+	subject { @user }
 
+	describe "with valid attributes" do
+	
 		it { should be_valid }
 
 		sym_arr = %i(name email id encrypted_password
@@ -22,31 +24,19 @@ describe User do
 	describe "with invalid attributes such as" do
 		describe "password" do
 			describe "being too short" do
-				before { @user = User.new(
-					name: "Example User", 
-					email: "user@example.com",
-					password: "foobar", 
-					password_confirmation: "foobar") }
+				before { @user.password = @password_confirmation = "foobar" }
 
 				it { should_not be_valid }
 			end
 
 			describe "being too long" do
-				before { @user = User.new(
-				name: "Example User", 
-				email: "user@example.com",
-				password: 'a'*100, 
-				password_confirmation: 'a'*100) }
+				before { @user.password = @password_confirmation = 'a'*100 }
 
 				it { should_not be_valid }
 			end
 
 			describe "mismatch" do
-				before { @user = User.new(
-				name: "Example User", 
-				email: "user@example.com",
-				password: "foobar12", 
-				password_confirmation: "foobar12") }
+				before { @user.password = 'mismatch' }
 
 				it { should_not be_valid }
 			end
@@ -54,21 +44,13 @@ describe User do
 
 		describe "name" do
 			describe "being empty" do
-				before { @user = User.new(
-				name: "", 
-				email: "user@example.com",
-				password: "foobar11", 
-				password_confirmation: "foobar11") }
+				before { @user.name = ' ' }
 
 				it { should_not be_valid }
 			end
 
 			describe "being too long" do
-				before { @user = User.new(
-				name: 'a' * 50, 
-				email: "user@example.com",
-				password: "foobar11", 
-				password_confirmation: "foobar11") }
+				before { @user.name = 'a' * 51 }
 
 				it { should_not be_valid }
 			end
@@ -76,33 +58,26 @@ describe User do
 
 		describe "email" do 
 			describe "being empty" do
-				before { @user = User.new(
-				name: "Funny Junk", 
-				email: "",
-				password: "foobar11", 
-				password_confirmation: "foobar11") }
+				before { @user.email = '' } 
 
 				it { should_not be_valid }
 			end
 
 			describe "being too long" do
-				before { @user = User.new(
-				name: "Funny Junk", 
-				email: 'b' * 50 + '.com',
-				password: "foobar11", 
-				password_confirmation: "foobar11") }
+				before { @user.email = 'b' * 50 + '.com' }
 
 				it { should_not be_valid }
 			end
 
 			describe "having bad format" do
-				before { @user = User.new(
-				name: "Jose Rizal", 
-				email: "bad_email_example",
-				password: "foobar11", 
-				password_confirmation: "foobar11") }
-
-				it { should_not be_valid }
+				it "should be invalid" do
+		      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+		                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+		      addresses.each do |invalid_address|
+		        @user.email = invalid_address
+		        expect(@user).not_to be_valid
+		      end
+	      end
 			end
 		end
 	end
