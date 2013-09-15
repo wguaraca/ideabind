3a0d04c6d62f4fd1789c55e292541dea269e0ccf
Edderic Ugaddan <edderic@gmail.com>
2013-09-15 12:16:39 -0400

    About to experiment with 'rails generate devise:views users'

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,8 +14,8 @@ shared_examples_for "failed login attempt" do
 	it { should have_selector('div.alert.alert-alert', 
 													text: 'Invalid') }	
 	it { should_not have_link('Edit Profile') }
-	it { should_not have_link('Settings') }
-	it { should_not have_link('Sign out') }
+	# it { should_not have_link('Settings') }  # 	*|* for future?
+	it { should_not have_link('Logout') }
 end
 
 describe "Authentication" do 
@@ -52,8 +52,50 @@ describe "Authentication" do
 				click_button 'Sign in'
 			end
 
-			it { should have_link('Edit Profile',   href: edit_user_registration_path) }
-			# it { should have_link('Edit Profile',   href: edit_user_registration_path) }
+			it { should have_link('Edit Profile', href: edit_user_registration_path) }
+			it { should have_link('Logout',     href: destroy_user_session_path) }
+			it { should have_selector('div.alert.alert-notice', text: 'Signed in successfully') }
+
+			# describe "accessing 'new'" do    # *|* Page seems to not render the view completely.
+			# 	before { get new_user_session_path }
+			# 	specify { response.should have_selector('div.alert.alert-alert', text: 'You are already signed in.') }
+			# end
+
+			# describe "accessing 'create'" do  # *|* Page seems to not render the view completely.
+			# 	before { post user_session_path }
+			# 	specify { response.should redirect_to(root_url) }
+			# end
+
+			describe "followed by signout" do
+				before { click_link 'Logout' }
+
+					it { should_not have_link('Edit Profile', href: edit_user_registration_path) }
+					it { should_not have_link('Logout',     href: destroy_user_session_path) }
+					it { should have_link("Sign up Now",			href: new_user_registration_path) }
+					it { should have_link("Login",            href: new_user_session_path) }
+					it { should have_selector('div.alert.alert-notice', text: 'Signed out successfully') }
+			end
+		end
+
+		describe "authorization" do
+			describe "for non-signed-in users" do
+				let(:user) { FactoryGirl.create(:user) }
+
+				describe "when attempting to visit a protected page" do 
+					before do
+						visit edit_user_password_path
+						fill_in 'user_email',    with: user.email
+						fill_in 'user_password', with: user.password
+						click_button "Sign in"
+					end
+
+					describe "after signing in" do
+						it 'should render the desired protected page' do
+							expect(page).to have_content "You are already signed in."
+						end
+					end
+				end
+			end
 		end
 	end
 end
