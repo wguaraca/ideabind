47d254c9c8f0af4e8a5f3b5236f7fa8a40930de6
Edderic Ugaddan <edderic@gmail.com>
2013-09-16 14:04:53 -0400

    Moved add_index to 'change' method

--- db/migrate/20130913153230_add_user_id_and_index_to_pins.rb
+++ db/migrate/20130913153230_add_user_id_and_index_to_pins.rb
@@ -1,7 +1,8 @@
 class AddUserIdAndIndexToPins < ActiveRecord::Migration
   def change
   	add_column :pins, :user_id, :integer
+  	add_index :pins, [:user_id, :created_at]
   end
 
-  add_index :pins, [:user_id, :created_at]
+  
 end
